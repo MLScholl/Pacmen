9a3a3b74987b3dd80adf5c7b21c542542f9bee2a
Michael Scholl <mlscholl@iastate.edu>
2017-11-30 15:23:23 -0600

    Adding from mine this time

---
+++ android/src/com/schojcir/pacmen/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.schojcir.pacmen;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.schojcir.pacmen.Pacmen;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Pacmen(), config);
+	}
+}
